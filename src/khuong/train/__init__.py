09b317ec0aebfe2e4473963c6845474963500dd7
.1 <huyenytmk2912@gmail.com>
2026-08-30 20:31:56 +0900

    feat: export checkpoint helpers

--- src/khuong/train/__init__.py
+++ src/khuong/train/__init__.py
@@ -1,5 +1,6 @@
 """Training utilities for Khuong."""
 
+from .checkpoint import load_checkpoint, save_checkpoint
 from .loop import train_step
 
-__all__ = ["train_step"]
+__all__ = ["train_step", "save_checkpoint", "load_checkpoint"]
