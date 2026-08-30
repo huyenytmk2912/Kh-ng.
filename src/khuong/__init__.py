b19fd048571a27325283a07564c7b9d2a7500570
.1 <huyenytmk2912@gmail.com>
2026-08-30 19:34:32 +0900

    feat: expose terminal capability from core package

--- src/khuong/__init__.py
+++ src/khuong/__init__.py
@@ -1,10 +1,15 @@
 """Khuong core package."""
 
 from .contracts import ArtifactManifest, ModelMetadata, TokenSequence, validate_compatibility
+from .terminal import TerminalCapability, TerminalPolicy, TerminalPolicyError, TerminalResult
 
 __all__ = [
     "ArtifactManifest",
     "ModelMetadata",
     "TokenSequence",
     "validate_compatibility",
+    "TerminalCapability",
+    "TerminalPolicy",
+    "TerminalPolicyError",
+    "TerminalResult",
 ]
