1061da30bbb5997f51166d033a0d50d047cebcb3
.1 <huyenytmk2912@gmail.com>
2026-08-30 19:48:30 +0900

    feat: expose model sizing estimates

--- src/khuong/model/__init__.py
+++ src/khuong/model/__init__.py
@@ -1,5 +1,11 @@
 """Khuong transformer model components."""
 
+from .estimate import ModelEstimate, estimate_parameters
 from .model import KhuongConfig, KhuongForCausalLM
 
-__all__ = ["KhuongConfig", "KhuongForCausalLM"]
+__all__ = [
+    "KhuongConfig",
+    "KhuongForCausalLM",
+    "ModelEstimate",
+    "estimate_parameters",
+]
